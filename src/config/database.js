432d49e5538f7412c067b6c1fcabead90a3dd421
Ygor <yrmuller@yahoo.com.br>
2020-02-18 23:17:46 -0300

    estrutura2.0

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
-module.export =
-{ dialect: 'postgres', 
+module.exports ={
+  dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
+  port: 5433,
   define: {
 	timestamps: true,
 	underscored: true,
