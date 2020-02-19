55e8f9d72702c65b8c971233d5aee57eb7ee31d3
Ygor <Ygor>
2020-02-19 17:27:57 -0300

    jwt

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,22 @@
 import { Router } from 'express';
-import UserController from './app/controllers/UserController';
+import User from './app/models/User';
+//import UserController from './app/controllers/Usercontroller';
+//import SessionController from './app/controllers/SessionController';
 
-const routes = new Router(); //importando variáve//rotas raíz 	
-	
-routes.post('/users', UserController.store);
+const routes = new Router();
+
+routes.get('/', async(req, res) => {
+    const user = await User.create({
+	    name:'nonoono',
+	    email:'nonon@nono.com.br',
+        password_hash:'123456',
+        
+        //routes.post('/users', UserController.store);
+        //routes.post('/sessions', UserController.store);
+    });
+    
+	return res.json(user);
+	});
+
 	
 export default routes;
